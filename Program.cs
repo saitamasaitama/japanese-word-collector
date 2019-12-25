a86530a5bd24a86bbc284c154257fb18d9ca9302
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2019-12-25 17:18:37 +0900

    the first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace japanese_word_collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
